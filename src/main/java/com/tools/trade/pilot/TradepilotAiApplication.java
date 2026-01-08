b91ecdd5aea51a7cc8541933916d6319044166d8
Manjinder Singh Rooprai <rooprai0044@gmail.com>
2026-01-09 00:08:30 +0530

    Added the author name

--- src/main/java/com/tools/trade/pilot/TradepilotAiApplication.java
+++ src/main/java/com/tools/trade/pilot/TradepilotAiApplication.java
@@ -3,6 +3,9 @@ package com.tools.trade.pilot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Manjinder Rooprai
+ */
 @SpringBootApplication
 public class TradepilotAiApplication {
 
--- src/test/java/com/tools/trade/pilot/TradepilotAiApplicationTests.java
+++ src/test/java/com/tools/trade/pilot/TradepilotAiApplicationTests.java
@@ -3,6 +3,9 @@ package com.tools.trade.pilot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author Manjinder Rooprai
+ */
 @SpringBootTest
 class TradepilotAiApplicationTests {
 
